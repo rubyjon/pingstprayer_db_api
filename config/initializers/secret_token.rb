1de6552962ed112c5308eaee6a760a07a9790a76
Jonatan Ekstedt <jonatan.ekstedt@gmail.com>
2012-10-20 14:21:09 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PingstprayerDbApi::Application.config.secret_token = '3376c9460a0f64dc2ca6f24fb07d91f82e7dc79e58e46465670eece9261a442d8630c8acebfef8ab3239f304196d7aa1e974542e12b3a633429b35483607f478'
